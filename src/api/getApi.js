43bc3e64c7f15bd353ea5828e28172d01843333d
ratul-sardar <ratulsardar4747@gmail.com>
2026-05-18 13:26:01 +0600

    made the fetch function for the all cars api

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -1,5 +1,5 @@
 export async function getAllCars() {
-  const res = await fetch(``);
+  const res = await fetch(`${process.env.SERVER_URI}/explore-cars/`);
   const data = await res.json();
 
   return data;
--- src/app/(public)/explore-cars/page.jsx
+++ src/app/(public)/explore-cars/page.jsx
@@ -1,3 +1,7 @@
-export default function ExploreCars() {
+import { getAllCars } from "@/api/getApi";
+
+export default async function ExploreCars() {
+  const allCars = await getAllCars();
+  console.log(allCars);
   return "Hi, from explore cars secton";
 }
